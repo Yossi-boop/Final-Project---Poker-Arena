62097da52de33e38495201faf6dbf8d32e9de71b
Yossi Daniels <danielsyossi@gmail.com>
2022-09-10 03:46:54 +0300

    Changes for platforms changes

---
+++ Front End/Server/Interface.cs
@@ -0,0 +1,27 @@
+﻿using Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    interface Interface
+    {
+        string Login(string i_Email, string i_Password);
+        string CreateUser(User i_NewUser);
+        string AddFriend(string i_Email, string i_FriendEmail);
+
+
+        User OpenDualMode(string i_Email);
+       
+
+
+
+        string CreateRoom(string i_ManagerEmail);
+        string JoinRoom(string i_PinNumber, string i_UserEmail);
+      
+
+    }
+}
